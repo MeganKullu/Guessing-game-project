8984a2bd54d9f2e3b416483ffcd1ee3938267417
MeganKullu <megan@DESKTOP-EFAHR9G.localdomain>
2023-03-10 16:35:46 +0300

    Second revision

--- guessing-game.js
+++ guessing-game.js
@@ -3,52 +3,52 @@ const readline = require("node:readline");
 
 const r1 = readline.createInterface({
     input: process.stdin,
-    output : process.stdout
+    output: process.stdout
 });
 
 let secretNumber;
 
-let askGuess = function() { r1.question("Enter a guess...", enteredNum => {
-    if (checkGuess(Number(enteredNum))) {
-        console.log("You win!")
-    }
-    else return askGuess();
-    r1.close();
-})
+let askGuess = function () {
+    r1.question("Enter a guess...", enteredNum => {
+        if (checkGuess(Number(enteredNum))) {
+            console.log("You win!")
+        }
+        else return askGuess();
+        r1.close();
+    })
 }
 
 
-let checkGuess = function(number) {
-    if ( number > secretNumber) {
-        console.log ("Too high")
+let checkGuess = function (number) {
+    if (number > secretNumber) {
+        console.log("Too high")
         return false;
     }
-    if ( number < secretNumber) {
-        console.log ("Too low")
+    if (number < secretNumber) {
+        console.log("Too low")
         return false;
     }
-    if ( number === secretNumber) {
-        console.log ("Correct!")
+    if (number === secretNumber) {
+        console.log("Correct!")
         return true;
     }
 }
 
 let randomInRange = (min, max) => {
-    return Math.floor((Math.random() * max) + min);
-}
+    return Math.floor((Math.random() * (max - min)) + min)
+};
 
 
-let askRange = function() {
+let askRange = function () {
     r1.question("What is your minimum number? ", minimum => {
         let min = Number(minimum);
         r1.question("What is your maximum number? ", maximum => {
-        let max = Number(maximum);
-        console.log("Your range is between " + min + " and " + max + ".")
-        secretNumber = randomInRange();
-        r1.close();
+            let max = Number(maximum);
+            console.log("Your range is between " + min + " and " + max + ".")
+            secretNumber = randomInRange(min, max);
+            askGuess();
         });
     });
-
 }
+askRange();
 
-randomInRange();
